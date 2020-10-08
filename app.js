e76d516f6e5b11d2125e2acbf8b71a7e9ea39345
kang-sctg <ckang@servercentral.com>
2020-10-08 11:40:39 -0500

    WIP- tinkering

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/healthz', (req, res) => res.status(200).json({ status: 'ok '}));
+app.get('/health', (req, res) => res.status(200).json({ status: 'ok '}));
 
 app.post('/dialogflow-webhook', (request, response) => {
   const agent = new WebhookClient({ request, response });
