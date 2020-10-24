c4d7bc198da8a6423148a3eaa4798a6fe8c7d1bb
beautyinfallingleaves <beautyinfallingleaves@gmail.com>
2020-10-24 18:28:17 -0500

    genericizes Jira ticket data; adds README draft

---
+++ README.md
@@ -0,0 +1,29 @@
+# Cbot
+This is a rough proof-of-concept application for experimenting with Google's Natural Language Understanding platform, [Dialogflow](https://cloud.google.com/dialogflow/es/docs), via a CLI chat bot which simulates letting you log hours against Jira tickets using natural language; e.g., *"Yesterday I worked an hour and a half on the video site's header and footer."*
+
+## Setup
+1. Follow the Dialogflow [setup instructions](https://cloud.google.com/dialogflow/es/docs/quick/setup) to set up a Google Cloud project. This includes setting up a Service Account for authentication.
+2. Clone this repo:  
+`git clone https://github.com/beautyinfallingleaves/cbot.git`
+3. Install dependencies:  
+`cd cbot && npm install`
+4. Rename `.env.example` to `.env` and replace values with those for your own Google project setup from step 1:
+    - `PROJECT_ID` should be the ID of your Google Cloud project
+    - `GOOGLE_APPLICATION_CREDENTIALS` should be the path to your Service Account credentials JSON file
+    - `MONKEYLEARN_TOKEN` is optional, if you want to experiment with ML keyword extraction from ticket descriptions- I found it didn't add value for this context; teaching Dialogflow with just the ticket titles was surprisingly effective. You can leave this variable as-is if not using MonkeyLearn.
+5. From the Dialogflow console, [import](https://cloud.google.com/dialogflow/es/docs/agents-settings#export) this repo's Dialogflow agent, `agent-backups/SimpleAlarm.zip`, into your Dialogflow project.
+6. This application uses Dialogflow's webhook intent fulfillment, which  requires that you serve it over `https` and update the Dialogflow Fulfillment Webhook URL in the console to your endpoint: `https://your_url/dialogflow-webhook`. [ngrok](https://ngrok.com/) is an easy way to quickly set up a public `https` URL.
+7. `npm start` to start the CLI agent and the Express server which handles intent fulfillment via webhook.
+
+Reading the [Dialogflow basics](https://cloud.google.com/dialogflow/docs/basics) as suggested by the setup guide is highly recommended in order to understand what this application is doing.
+
+*Note: the `actions-on-google` libary we use to handle webhook fulfillment requires Node v10.*
+
+## Using Cbot
+Try typing *"hi", "heya cbot",* or similar on the CLI- Cbot should recognize most normal greetings and suggest you ask it about finding tickets or logging hours.
+
+Now try asking it to find tickets with *"find tickets", "load my tickets", "fetch my tickets",* or other permutations. This is just loading dummy ticket data (one day we'll do an actual fetch from Jira...), but under the hood, it's using Dialogflow Session Entities to train Cbot to recognize you speaking naturally/colloquially about these tickets, in order to find the right one when you ask it to log hours. **This step is necessary in order for ticket lookup to work.**
+
+Finally, the fun part: ask Cbot to log some quantity of hours worked on some date against one of the fetched tickets. For example: *"Standup took 15min today", "Yesterday I worked an hour and a half on Logic Monitor," or, "I updated the video site's header for 20min last Tuesday."* Try logging hours in a bunch of different ways- Cbot should have a decent success rate at identifying the ticket, date, and quantity of time worked.
+
+This is just a starting point- the agent could use *a lot* more fleshing out. If you find your requests are missing the mark, it's a great opportunity to experiment with fine tuning the agent in Dialogflow, such as adding additional training phrases, or even coming up with a better way of teaching it about dyanmically fetched tickets (right now, it's using only the ticket title to teach Dialogflow- a bit dubious).
--- jira-cases.js
+++ jira-cases.js
@@ -1,41 +1,41 @@
 const dummyTickets = [
   {
-    name: 'SCOPE-2863',
+    name: 'POWER-1346',
     title: 'Ability to add a pair (primary/redundant) for Local Power Circuits',
     // description: 'As Is process for Remote Power Circuits: In Scope > left menu -> add new asset -> add remote power circuit -Remote Power circuits can add a power circuit as a pair. (Snip attached) -On this form, if selected yes, then a "Primary power circuit details" and "Redundant power circuit details" sections will drop down. -On this form, if selected no, then only a "power circuit details" section will drop down.  *Enhancement desired for Local Power Circuits: * -In Scope> left menu -> add new asset -> create circuit pair  -Local power circuits should have this same pairing capability. There should be an option (like the one above) to pair a power circuit into primary/redundant sources.',
   },
   {
-    name: 'DIGIME-137',
+    name: 'VIDEO-137',
     title: 'updates to header and footer',
     // description: 'Remove header completely Footer should only be the bottom 2 lines of what is in the spec. The rest of the header and footer should not appear on any page.',
   },
   {
-    name: 'SCOPE-2866',
+    name: 'POWER-2866',
     title: 'Ability to sever an existing Power Circuit Pair, resulting in two standalone Primary-type circuits',
-    // description: "Andy Thomas requires the ability to sever an existing Power Circuit Pair relationship, resulting in two standalone 'Primary'-type circuits, so that he can individually repurpose circuits which were originally linked as a redundancy pair for use in other scenarios (such as one of the circuits from the pair being used as a supplemental, tertiary circuit for another circuit pair). This amounts to creating a button in the UI which performs the pair-severing behavior we recently scripted for several existing circuits as part of https://tgsupport.atlassian.net/browse/SCOPE-2776.",
+    // description: "Randy Grana requires the ability to sever an existing Power Circuit Pair relationship, resulting in two standalone 'Primary'-type circuits, so that he can individually repurpose circuits which were originally linked as a redundancy pair for use in other scenarios (such as one of the circuits from the pair being used as a supplemental, tertiary circuit for another circuit pair). This amounts to creating a button in the UI which performs the pair-severing behavior we recently scripted for several existing circuits as part of POWER-2776.",
   },
   {
-    name: 'SCOPE-2868',
+    name: 'MONITOR-2868',
     title: 'Logic Monitor version reverting to V1',
     // description: 'Bug: As a MS user, when I update the logic monitor credentials the LM version gets set to v1, even if the dropdown shows v2. I need to change the dropdown to v1 then back to v2 for v2 to be saved.',
   },
   {
-    name: 'DIGIME-141',
+    name: 'VIDEO-141',
     title: 'Playlist management API & backend work',
     // description: 'As a CMS user, I need to be able to manage playlists for my company. I need to be able to update the playlist name, add jobs to a playlist, remove jobs to a playlist, and update the order of jobs in a playlist. I also need to be able to delete a playlist Need to create API routes and database changes to support this functionality'
   },
   {
-    name: 'SCOPE-2829',
+    name: 'CONNS-2829',
     title: 'Remove incomplete connections',
     // description: 'Create script to find all incomlete connections for switches and servers and remove them',
   },
   {
-    name: 'INTERNAL-668',
+    name: 'INTERNAL-448',
     title: 'Standup & Sprint Planning',
     // description: 'Standup & Sprint Planning',
   },
   {
-    name: 'INTERNAL-26',
+    name: 'INTERNAL-236',
     title: 'Internal - Time Tracking and Ticket clearing',
     // description: 'Internal - Time Tracking and Ticket clearing',
   },
